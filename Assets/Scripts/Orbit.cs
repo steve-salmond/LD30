576ce7f52e674fc8ce271dbbe6f2ad1ed9287915
Steve Salmond <steve.salmond@smallworlds.com>
2014-08-23 13:42:53 +1200

    Initial commit.

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour 
+{
+
+	// Properties
+	// -----------------------------------------------------
+
+	/** Axis of rotation. */
+	public Vector3 Axis;
+
+	/** Rotation speed (degrees per second). */
+	public float Speed;
+
+
+	// Members
+	// -----------------------------------------------------
+
+	/** Cached transform. */
+	private Transform t;
+
+	// Use this for initialization
+	void Start() 
+	{
+		t = transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() 
+	{
+		t.Rotate (Axis, Speed * Time.deltaTime, Space.Self);
+	}
+}
